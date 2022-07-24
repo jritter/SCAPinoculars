4d4d0c4efdf1bb097dc114f24183855ccb567ea9
Juerg Ritter <jritter@redhat.com>
2022-07-24 12:52:54 +0200

    reformating

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-
 func main() {
 
 	result := openscap_report_parser.ParseReport("resources/arf.xml")
@@ -19,19 +18,17 @@ func main() {
 		if result.Result != "notselected" {
 			fmt.Printf("ID: %s\tResult: %s\n", result.IdRef, result.Result)
 			gauge := promauto.NewGauge(prometheus.GaugeOpts{
-				Name: "openscap_results",
-				Help: "OpenSCAP Results",
+				Name:        "openscap_results",
+				Help:        "OpenSCAP Results",
 				ConstLabels: prometheus.Labels{"openscap_ref": result.IdRef, "severity": result.Severity},
 			})
-			if (result.Result == "fail"){
+			if result.Result == "fail" {
 				gauge.Set(0)
 			} else {
 				gauge.Set(1)
 			}
 		}
 	}
-
-	
 
 	fs := http.FileServer(http.Dir("resources/"))
 	http.Handle("/resources/", http.StripPrefix("/resources/", fs))
